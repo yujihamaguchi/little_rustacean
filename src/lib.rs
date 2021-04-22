f36202146cda809fddc609b2cbbd2e85957b2535
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2021-04-22 12:48:56 +0900

    tweak

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,13 @@ fn my_sum(ns: &[usize]) -> usize {
 
 // https://doc.rust-jp.rs/book-ja/ch08-03-hash-maps.html
 // 0020. Write a function named my_mean calcurating the average of elements of Vec.
+#[allow(dead_code)]
+fn my_mean(ns: &[usize]) -> Option<usize> {
+    match ns.len() {
+        0 => None,
+        n => Some(my_sum(ns) / n),
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -21,5 +28,11 @@ mod tests {
         assert_eq!(0, my_sum(&vec![]));
         assert_eq!(1, my_sum(&vec![1]));        
         assert_eq!(6, my_sum(&vec![1, 2, 3]));
+    }
+    #[test]
+    fn test_my_mean() {
+        assert_eq!(None, my_mean(&vec![]));
+        assert_eq!(Some(2), my_mean(&vec![2]));
+        assert_eq!(Some(2), my_mean(&vec![1, 2, 3]));
     }
 }
