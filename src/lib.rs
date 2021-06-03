d8633d4860010db718e286cb71b954749f73865c
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2021-06-03 09:22:00 +0900

    tweak

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,10 @@ fn my_mode(ns: &[usize]) -> Option<usize> {
         for &n in ns {
             *occurrences.entry(n).or_insert(0) += 1;
         }
-        let (&k, _) = occurrences.iter().max_by(|(_, v1), (_, v2)| v1.cmp(v2)).unwrap();
+        let (&k, _) = occurrences
+            .iter()
+            .max_by(|(_, v1), (_, v2)| v1.cmp(v2))
+            .unwrap();
         Some(k)
     }
 }
