3394456aabf0b09d36b66f5977ffb92009bce51a
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2021-06-07 17:37:05 +0900

    tweak

---
+++ src/helper.rs
@@ -0,0 +1,12 @@
+use std::time::SystemTime;
+
+#[allow(dead_code)]
+pub fn execution_seconds<F>(f: F) -> u64
+where
+    F: Fn() -> (),
+{
+    let start = SystemTime::now();
+    (f());
+    let end = SystemTime::now();
+    end.duration_since(start).unwrap().as_secs()
+}
